2f3136fe8ea5a0946834d406a5b1b06ec9d3db9a
Monika-Lis <lis.monikak@gmail.com>
2024-02-04 13:09:18 +0000

    updating tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,53 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+galleryItems.forEach(function (galleryItem) {
+  const codeHTML = `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${galleryItem.original}">
+        <img
+          class="gallery__image"
+          src="${galleryItem.preview}"
+          data-source="${galleryItem.original}"
+          alt="${galleryItem.description}"
+        />
+      </a>
+    </div>
+  `;
+
+  gallery.insertAdjacentHTML("beforeend", codeHTML);
+});
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const target = event.target;
+  const isImage = target.classList.contains("gallery__image");
+
+  if (isImage) {
+    const imageSrc = target.getAttribute("data-source");
+
+    const instance = basicLightbox.create(
+      `
+      <img src="${imageSrc}" width="800" height="600">
+    `,
+      {
+        onShow: (instance) => {
+          window.addEventListener("keydown", onEscPress);
+        },
+        onClose: (instance) => {
+          window.removeEventListener("keydown", onEscPress);
+        },
+      }
+    );
+
+    function onEscPress(event) {
+      if (event.key === "Escape") {
+        instance.close();
+      }
+    }
+
+    instance.show();
+  }
+});
